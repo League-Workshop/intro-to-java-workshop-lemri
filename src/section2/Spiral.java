0df0becc829375c9f60ed48f34e3aeb28990685e
philliphjhuang <30792325+philliphjhuang@users.noreply.github.com>
2019-01-04 13:11:10 -0800

    Day3

--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -7,36 +7,48 @@ public class Spiral {
 	public static void main(String[] args) {
 		// 1. Create a new Robot
 		Robot terminator=new Robot("batman");
-		Robot r2d2=new Robot("mini");
-		Robot r2g=new Robot("mini");
+	
+	
 		terminator.setSpeed((int)100);
-		r2d2.setSpeed((int)100);
-		r2g.setSpeed((int)100);
+
 		terminator.penDown();
-	r2g.penDown();
-		r2d2.penDown();
-	terminator.setRandomPenColor();
-	for (int i = 5; i < 100; i++) {
+
+	for (int i = 5; i < 1000; i++) {
+		terminator.penUp();
 		terminator.move(200);
-	r2d2.turn(100);
-	r2d2.move(50);
+		terminator.penDown();
 		terminator.turn(75);
 		terminator.move(55);
 		terminator.turn(67);
 		terminator.move(54);
 		terminator.turn(90);
-		r2d2.turn(90);
+		terminator.move(56);
 		terminator.move(50);
+		terminator.move(50);
+		terminator.turn(35);
 		terminator.turn(90);
 		terminator.move(6);
 		terminator.turn(70);
 		terminator.move(62);
-		r2d2.move(150);
 		terminator.turn(30);
-		r2g.move(25);
-		r2g.turn(95);
-		 r2d2.setRandomPenColor();
-		 r2g.setRandomPenColor();
+		terminator.penUp();
+		terminator.move(200);
+		terminator.penDown();
+		terminator.turn(75);
+		terminator.move(55);
+		terminator.turn(67);
+		terminator.move(54);
+		terminator.turn(90);
+		terminator.move(56);
+		terminator.move(50);
+		terminator.move(50);
+		terminator.turn(35);
+		terminator.turn(90);
+		terminator.move(6);
+		terminator.turn(70);
+		terminator.move(62);
+		terminator.turn(30);
+
 		 terminator.setRandomPenColor();
 	}
 	}
--- src/section5/ScaryMaze.java
+++ src/section5/ScaryMaze.java
@@ -21,17 +21,19 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 	BufferedImage maze;
 	final int frameWidth = 800;
 	final int frameHeight = 600;
+	private int mouseColor;
+	private Object scary;
 
 	ScaryMaze() throws Exception {
 		//1. Use this online tool to make a maze image and drop it into your section5 package: http://pixlr.com/editor/
-		//maze = ImageIO.read(getClass().getResource("standardMaze.jpg"));
+		maze = ImageIO.read(getClass().getResource("scarymaze.png"));
 		//2. Change the line of code above so that it matches your maze's file name
 		
 		//3. Set the mouse pointer to the start of your maze using:
-		//new Robot().mouseMove(45, 95);
+		new Robot().mouseMove(30,50);
 		
 		//4. Add a mouse motion listener using:
-		//addMouseMotionListener(this);
+		addMouseMotionListener(this);
 		
 	}
 
@@ -41,41 +43,40 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseY = e.getY();
 		int mouseColor = maze.getRGB(mouseX, mouseY);
 		//5. Print the mouseColor variable 
-		
+		System.out.println(mouseColor);
 		//6.  Run your program and put your mouse over the background to find out what color it is
-		
+		int backgroundColor1=-3285959;
+		int backgroundColor2=-43230;
+
 		//7. Create an int variable that holds the background color. 
 		
 		//8. If the mouse falls off the path (if it is on the background)
-	
-				// call the scare method
+	 if(mouseColor==backgroundColor1 || mouseColor==backgroundColor2)
+				scare();
 		
 		//13. If the mouse is on the end color
-		
+		if( mouseColor==-1754827){
 				// pop up a message to tell them they won
-		
+ JOptionPane.showMessageDialog(null, ("You Won"));}
+
 	}
 
 	private void scare() {
 		System.out.println("BOO!");
 		//9. Find a scary sound and put it in the section5 package where you put your maze picture. You can find a sound on freesound.org. Log in as leagueofamazing/code4life.
-		
+
 		//10. Use the code below to load your sound.  Change the file name to match the name of your sound file.  
-		//AudioClip sound = JApplet.newAudioClip(getClass().getResource("standardScarySound.wav"));
-		
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource("scary.wav"));
+ sound.play();
 		//11. Play the scary sound. Hint: type "sound" and then a period.		
-		
+
 		//12. Drop an image into your section5 package, and use the showScaryImage method to scare your victim!
-		
+		showScaryImage("scary.jpg");
 	}
 
-	private void showScaryImage(String imageName) {
-		try {
-			maze = ImageIO.read(getClass().getResource(imageName));
-		} catch (Exception e) {
-			System.err.println("Couldn't find this image: " + imageName);
-		}
-		repaint();
+	private void play(String string) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -100,6 +101,15 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 
 	@Override
 	public void mouseDragged(MouseEvent e) {}
+
+	private void showScaryImage(String imageName) {
+		try {
+			maze = ImageIO.read(getClass().getResource(imageName));
+		} catch (Exception e) {
+			System.err.println("Couldn't find this image: " + imageName);
+		}
+		repaint();
+	}
 
 }
 
